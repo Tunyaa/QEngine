a6f7707f25b254a9bdfa9089f663221da2036ebb
Tunyaa <oldcampthunder@gmail.com>
2025-06-26 22:50:10 +0300

    +0

---
+++ src/main/java/com/qengine/qengine/QengineApplication.java
@@ -0,0 +1,13 @@
+package com.qengine.qengine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QengineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QengineApplication.class, args);
+	}
+
+}
